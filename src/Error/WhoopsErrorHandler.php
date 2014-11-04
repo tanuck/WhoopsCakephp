ee18eac93d92caa7184a406a983ad65bc5a5d910
oldskool <oldskool@fluxbb.org>
2014-11-04 23:36:49 +0100

    Updating handler method visibility to match parent class

--- src/Error/WhoopsErrorHandler.php
+++ src/Error/WhoopsErrorHandler.php
@@ -6,13 +6,13 @@ use Cake\Error\BaseErrorHandler;
 
 class WhoopsErrorHandler extends BaseErrorHandler {
 
-	public function _displayError($error, $debug) {
+	protected function _displayError($error, $debug) {
 		$Whoops = new \Whoops\Run();
 		$Whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
 		return $Whoops->handleError(Configure::read('Error.errorLevel'), $error);
 	}
 
-	public function _displayException($exception) {
+	protected function _displayException($exception) {
 		$Whoops = new \Whoops\Run();
 		$Whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler());
 		return $Whoops->handleException($exception);
